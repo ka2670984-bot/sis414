3921cee9a7f2ce00acc5b1d142b7b24f01cf3ba0
Alex Kevin <ka2670984@gmail.com>
2026-04-04 09:03:59 -0400

    fix hello post y get

--- src/main/java/com/sis414/tarea/hello.java
+++ src/main/java/com/sis414/tarea/hello.java
@@ -1,12 +1,29 @@
 package com.sis414.tarea;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 @RestController
 
 public class hello {
-@GetMapping("/hello")
+
+    ArrayList<String> list = new ArrayList<>();
+
+    @GetMapping("/hello")
     public String Gethello() {
-        return " hello";
+        String text = "";
+        for (int i = 0; i < list.size(); i++) {
+            text = text + list.get(i);
+        }
+        return text;
+    }
+    @PostMapping("/hello")
+    public String Posthello(@RequestBody String variable) {
+        list.add(variable);
+        return variable;
     }
 }
